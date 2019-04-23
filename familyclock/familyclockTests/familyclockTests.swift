6a5bca2050a0872d6e25403dab063d62609b7a81
Alexis Ferrante <alexisferrante22@gmail.com>
2019-04-23 16:30:07 -0400

    created project

---
+++ familyclock/familyclockTests/familyclockTests.swift
@@ -0,0 +1,34 @@
+//
+//  familyclockTests.swift
+//  familyclockTests
+//
+//  Created by Alexis Ferrante on 4/23/19.
+//  Copyright © 2019 Alexis Ferrante. All rights reserved.
+//
+
+import XCTest
+@testable import familyclock
+
+class familyclockTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
