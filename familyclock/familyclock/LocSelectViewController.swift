c617c861c0b639af9dd329321f07e88f02395ea8
Brad <Genespark@users.noreply.github.com>
2019-05-09 12:05:05 -0400

    changes

--- familyclock/familyclock/LocSelectViewController.swift
+++ familyclock/familyclock/LocSelectViewController.swift
@@ -14,34 +14,53 @@ class LocSelectViewController: UIViewController, CLLocationManagerDelegate  {
 
     @IBOutlet weak var location: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var selectLabel: UILabel!
+    @IBOutlet weak var done: UIButton!
     
-    @IBAction func tapGesture(_ sender: Any) {
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
         if (sender.state == .ended) {
-            let locationInView = sender.location(in: mapView)
-            let tappedCoordinate = mapView.convert(locationInView, toCoordinateFrom: mapView)
-            
-            if (coordinates.count < 5) {
-                coordinates.append(tappedCoordinate)
-                location.text = locations[coordinates.count]
-            }
-            print(coordinates)
-            print("")
-            dropPin(name: locations[coordinates.count-1], place: tappedCoordinate)
-            if (coordinates.count >= 5) {
-                print("continue")
-                //connect to next page here
+            if (index < locations.count) {
+                let locationInView = sender.location(in: mapView)
+                let tappedCoordinate = mapView.convert(locationInView, toCoordinateFrom: mapView)
+                coordinates[index] = tappedCoordinate
+                dropPin(name: locations[index], place: tappedCoordinate)
+                index += 1
+                if (index > locations.count-1) {
+                    done.isHidden = false
+                    location.isHidden = true
+                    selectLabel.text = "Tap 'next' to proceed"
+                } else {
+                    location.text = locations[index]
+                }
             }
         }
     }
     
-    var locations = ["home", "work", "school", "dentist", "gym", "gym"]
+    @IBAction func prevButton(_ sender: Any) {
+        if (index > 0) {
+            index -= 1
+            mapView.removeAnnotation(annotations[index])
+            location.text = locations[index]
+            if (done.isHidden == false) {
+                done.isHidden = true
+                selectLabel.text = "Select Location:"
+                location.isHidden = false
+            }
+        }
+    }
+    @IBAction func nextButton(_ sender: Any) {
+        //segue here!
+    }
+    
+    var index = 0
     //coordinates stored in the coordinates variable will be in the order of: home, work, school, dentist, gym
-    var coordinates: [CLLocationCoordinate2D] = []
-    var locationManager = CLLocationManager()
-    let newPin = MKPointAnnotation()
+    var locations = ["Home", "Work", "School", "Dentist", "Gym"]
+    var coordinates = [CLLocationCoordinate2D(),CLLocationCoordinate2D(),CLLocationCoordinate2D(),CLLocationCoordinate2D(),CLLocationCoordinate2D()]
+    var annotations = [MKPointAnnotation(),MKPointAnnotation(),MKPointAnnotation(),MKPointAnnotation(),MKPointAnnotation()]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        done.isHidden = true
         checkAuth()
         locationManager.delegate = self
     }
@@ -71,16 +90,4 @@ class LocSelectViewController: UIViewController, CLLocationManagerDelegate  {
         pin.title = name
         mapView.addAnnotation(pin)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
